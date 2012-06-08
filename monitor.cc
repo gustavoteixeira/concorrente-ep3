790d0b92cfe4516cfdbe076ebab1f96a1ec43925
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2012-06-08 17:59:47 -0300

    Magia?

--- monitor.cc
+++ monitor.cc
@@ -1,11 +1,48 @@
 #include "monitor.h"
+#include <queue>
 
-Monitor::Monitor() {
-    sem_init(&semaforo_, 0, 1); 
+static int GET_ID_POR_MAGIA() { return 42; }
+
+
+struct MonitorCond {
+    std::queue<int> delay;
+};
+
+Monitor::Monitor(int N) : N_(N) {
+    sem_init(&semaforo_, 0, 1);
+    processos_privado_ = new sem_t[N];
+    for(int i = 0; i < N; ++i)
+        sem_init(&processos_privado_[i], 0, 1);
 }
 
 Monitor::~Monitor() {
     sem_destroy(&semaforo_);
+    for(int i = 0; i < N_; ++i)
+        sem_init(&processos_privado_[i], 0, 1);
+}
+
+void Monitor::wait(MonitorCond* cv) {
+    entrada();
+
+    int id = GET_ID_POR_MAGIA();
+    cv->delay.push(id);
+    saida();
+    sem_wait(&processos_privado_[id]);
+    entrada();
+
+    saida();
+}
+
+void Monitor::signal(MonitorCond* cv) {
+    entrada();
+
+    if(!cv->delay.empty()) {
+        int id = cv->delay.front();
+        cv->delay.pop();
+        sem_post(&semaforo_);
+    }
+
+    saida();
 }
 
 void Monitor::entrada() {
--- monitor.h
+++ monitor.h
@@ -3,17 +3,25 @@
 
 #include <semaphore.h>
 
+struct MonitorCond;
+
 class Monitor {
   public:
-    Monitor();
+    Monitor(int N);
     ~Monitor();
 
-    //empty(cv), wait(cv), wait(cv,rank), signal(cv), signal all(cv), minrank(cv)
+    //empty(cv),  wait(cv,rank), signal all(cv), minrank(cv)
+
+    void wait(MonitorCond* cv);
+    void signal(MonitorCond* cv);
+
   private:
     void entrada();
     void saida();
 
+    int N_;
     sem_t semaforo_;
+    sem_t *processos_privado_;
 };
 
 #endif /* MONITOR_H_ */
