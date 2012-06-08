28a3db525ddbd67acdaaae05f72156bea1f124d6
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2012-06-08 16:36:51 -0300

    Moar data.

--- carro.h
+++ carro.h
@@ -2,6 +2,10 @@
 #define CARRO_H_
 
 class Carro {
+  public:
+    int id() const { return id_; }
+  private:
+    int id_;
 };
 
 #endif /* CARRO_H_ */
--- passageiro.h
+++ passageiro.h
@@ -2,6 +2,12 @@
 #define PASSAGEIRO_H_
 
 class Passageiro {
+  public:
+    int id() const { return id_; }
+    bool bilhete_dourado() const { return bilhete_dourado_; }
+  private:
+    int id_;
+    bool bilhete_dourado_;
 };
 
 #endif /* PASSAGEIRO_H_ */
