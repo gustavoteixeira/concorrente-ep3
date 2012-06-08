9d328340e10edc98537541443bb09af71216cde4
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2012-06-08 17:37:31 -0300

    Temos um semáforo!

--- monitor.cc
+++ monitor.cc
@@ -1,4 +1,9 @@
 #include "monitor.h"
 
 Monitor::Monitor() {
+    sem_init(&semaforo_, 0, 0); 
+}
+
+Monitor::~Monitor() {
+    sem_destroy(&semaforo_);
 }
--- monitor.h
+++ monitor.h
@@ -1,9 +1,16 @@
 #ifndef MONITOR_H_
 #define MONITOR_H_
 
+#include <semaphore.h>
+
 class Monitor {
   public:
     Monitor();
+    ~Monitor();
+
+    //empty(cv), wait(cv), wait(cv,rank), signal(cv), signal all(cv), minrank(cv)
+  private:
+    sem_t semaforo_;
 };
 
 #endif /* MONITOR_H_ */
