fd12dc11f3a0ceb7db501a6ef49b7d61b7436ea0
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2012-06-08 16:26:51 -0300

    .h idiotas.

---
+++ carro.h
@@ -0,0 +1,7 @@
+#ifndef CARRO_H_
+#define CARRO_H_
+
+class Carro {
+};
+
+#endif /* CARRO_H_ */
---
+++ passageiro.h
@@ -0,0 +1,7 @@
+#ifndef PASSAGEIRO_H_
+#define PASSAGEIRO_H_
+
+class Passageiro {
+};
+
+#endif /* PASSAGEIRO_H_ */
